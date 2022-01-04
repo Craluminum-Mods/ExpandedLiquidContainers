883c87fdc6e0784509c179d6f478e24efd719e22
Craluminum2413 <beware.14b@gmail.com>
2022-01-04 17:40:59 +0200

    Chest liquid container

--- src/Config/List.cs
+++ src/Config/List.cs
@@ -13,17 +13,18 @@ namespace ExpandedLiquidContainers.List
 	{
 		public static ExpandedLiquidContainersConfig Loaded { get; set; } = new ExpandedLiquidContainersConfig();
 
+		public Part AnvilMold { get; set; } = new Part(true, false, 30);
+		public Part Chest { get; set; } = new Part(true, false, 50);
 		public Part CookingPot { get; set; } = new Part(true, false, 10);
 		public Part Crock { get; set; } = new Part(true, false, 8);
 		public Part Crucible { get; set; } = new Part(true, false, 8);
 		public Part Flowerpot { get; set; } = new Part(true, false, 10);
+		public Part Henbox { get; set; } = new Part(true, false, 8);
 		public Part Planter { get; set; } = new Part(true, false, 25);
 		public Part StorageVessel { get; set; } = new Part(true, false, 50);
-		public Part AnvilMold { get; set; } = new Part(true, false, 30);
-		public Part Henbox { get; set; } = new Part(true, false, 8);
-		public Part WoodenPan { get; set; } = new Part(true, false, 8);
-		public Part TroughSmall { get; set; } = new Part(true, false, 16);
 		public Part TroughLarge { get; set; } = new Part(true, false, 40);
+		public Part TroughSmall { get; set; } = new Part(true, false, 16);
+		public Part WoodenPan { get; set; } = new Part(true, false, 8);
 
 		private static readonly PropertyInfo[] propertyInfos = typeof(ExpandedLiquidContainersConfig).GetProperties()
 			.Where(propertyInfo => propertyInfo.PropertyType == typeof(Part)).ToArray();
